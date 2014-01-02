88e2df8d2540b51eb0362de84a6fb2be84f20ec3
bill <bill@blah.com>
2014-01-01 16:57:33 -0800

    Fixing stuff to push to github.

--- src/main.c
+++ src/main.c
@@ -43,6 +43,9 @@ int main(int argc, char **argv)
   }
 
   uint8_t macAddr[6] = { 0x24, 0x52, 0x81, 0x93, 0x19, 0x22 };
+
+  mac_set_frame(&mdev.recvFrame, pktbuff, TAP_BUFFER_SIZE);
+  mac_set_frame(&mdev.sendFrame, outbuff, TAP_BUFFER_SIZE);
   mac_init(&mdev, macAddr);
   iph_init(&iph, &mdev);
   iph_set_ip4addr(&iph, IPV4_ADDR(10, 5, 1, 1), 0xffffffff);
@@ -55,7 +58,7 @@ int main(int argc, char **argv)
       exit(-1);
     }
     mac_clear_frame(&mdev.recvFrame);
-    mac_write_frame(&mdev.recvFrame, pktbuff, nread, 1);
+    //mac_write_frame(&mdev.recvFrame, pktbuff, nread, 1);
 
     uint8_t *ra = mac_frame_header(&mdev.recvFrame)->srcAddr;
     printf("Pkt %i read from 0x%hhx%hhx%hhx%hhx%hhx%hhx %hx 0x%02hhx%02hhx%02hhx%02hhx.\n", mac_payload_len(&mdev.recvFrame), ra[0], ra[1], ra[2], ra[3], ra[4], ra[5], NTOHS(mac_frame_header(&mdev.recvFrame)->wType), pktbuff[0], pktbuff[1], pktbuff[2], pktbuff[3]);
